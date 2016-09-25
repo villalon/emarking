6f8d30ef506ab4a690d379749c5d991af7141a6a
Hans Cristian Jeria Díaz <hansjeria@gmail.com>
2016-09-25 15:31:41 -0400

    Update version.php
    remove merge comments

--- version.php
+++ version.php
@@ -28,10 +28,6 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-<<<<<<< HEAD
 $plugin->version = 2016091901; // The current module version (Date: YYYYMMDDXX).
-=======
-$plugin->version = 2016091600; // The current module version (Date: YYYYMMDDXX).
->>>>>>> upstream/master
 $plugin->requires = 2013110500; // Requires this Moodle version.
 $plugin->component = 'mod_emarking'; // To check on upgrade, that module sits in correct place.
